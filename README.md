bde916068b0635a8b2461b0bf0cbfa431ebe1433
Stefan Thoss <stefanthoss@users.noreply.github.com>
2021-03-07 19:48:47 -0800

    Add index.html

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This project is using [io-2012-slides](https://code.google.com/archive/p/io-2012-slides) which is licensed under the
 [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+
+## Local Web Server
+
+Use `bash serve.sh 3000` to serve the slides locally at <http://localhost:3000>.
--- serve.sh
+++ serve.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 #
-# Starts a basic web server on the port specified.
-# 
-# ./serve.sh 3000 -> http://localhost:3000
+# Copyright 2012 Eric Bidelman
 #
-# Copyright 2012 Eric Bidelman <ebidel@gmail.com>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 port=$1
 if [ $#  -ne  1 ]
@@ -19,4 +27,4 @@ else
   open=xdg-open
 fi
 
-$open http://localhost:$port/template.html && python -m http.server $port;
+$open http://localhost:$port/index.html && python -m http.server $port;
