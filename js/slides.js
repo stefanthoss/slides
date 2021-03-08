dcaca741817cf234c27e795989b49be50c095e8d
Stefan Thoss <stefanthoss@users.noreply.github.com>
2021-03-07 22:18:53 -0800

    Move template to subfolder

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,7 @@
-require(['order!../slide_config', 'order!modernizr.custom.45394',
-         'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
+require(['order!modernizr.custom.45394',
+         'order!prettify/prettify',
+         'order!hammer',
+         'order!slide-controller',
          'order!slide-deck'], function(someModule) {
 
 });
